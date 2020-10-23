2d0c15d436dc98e2c50892796d653dbe16b86d7d
Burak Karahan <burak.karahan@mail.ru>
2020-10-23 18:40:47 +0300

    Add changes

---
+++ elpa/flutter-20200501.1528/flutter-pkg.el
@@ -0,0 +1,12 @@
+(define-package "flutter" "20200501.1528" "Tools for working with Flutter SDK"
+  '((emacs "24.4"))
+  :keywords
+  '("languages")
+  :authors
+  '(("Aaron Madlon-Kay"))
+  :maintainer
+  '("Aaron Madlon-Kay")
+  :url "https://github.com/amake/flutter.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
